7410df378180ce4b1c02d38b183a9cd2832008d2
Goat Herder <goatherder@mailbox.org>
2023-03-25 10:51:45 +0100

    llama adds some leading axes that confuse the triu implementation

--- llama/workaround.py
+++ llama/workaround.py
@@ -2,8 +2,7 @@ import torch
 
 
 def triu(x, diagonal=0):
-    '''torch.triu is not well supported by the onnx exporter, so we implement a fallback here'''
-    l = x.shape[0]
+    l = x.shape[-1]
     arange = torch.arange(l, device=x.device)
     mask = arange.expand(l, l)
     arange = arange.unsqueeze(-1)
