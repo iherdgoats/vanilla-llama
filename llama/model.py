f704113cb9949d0440b48770798839da9140eec2
Goat Herder <goatherder@mailbox.org>
2023-03-25 19:11:27 +0100

    Clean up complex matmul impl

--- llama/model.py
+++ llama/model.py
@@ -9,7 +9,6 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
-from llama.complex import ComplexTensorPair, _view_as_complex, _view_as_real
 from llama.workaround import triu
 
 
@@ -44,28 +43,37 @@ def precompute_freqs_cis(dim: int, end: int, theta: float = 10000.0):
     freqs = 1.0 / (theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))
     t = torch.arange(end, device=freqs.device)  # type: ignore
     freqs = torch.outer(t, freqs).float()  # type: ignore
-    freqs_cis = ComplexTensorPair(torch.cos(freqs), torch.sin(freqs))
-    return freqs_cis
+    return torch.stack([torch.cos(freqs), torch.sin(freqs)], dim=-1)
 
 
-def reshape_for_broadcast(freqs_cis: ComplexTensorPair, x: ComplexTensorPair):
+def reshape_for_broadcast(freqs_cis: torch.Tensor, x: torch.Tensor):
     ndim = x.ndim
     assert 0 <= 1 < ndim
-    assert freqs_cis.shape == (x.shape[1], x.shape[-1])
-    shape = [d if i == 1 or i == ndim - 1 else 1 for i, d in enumerate(x.shape)]
+    assert freqs_cis.shape == (x.shape[1], x.shape[-2], x.shape[-1])
+    shape = [d if i == 1 or i == ndim - 2 or i == ndim - 1 else 1 for i, d in enumerate(x.shape)]
     return freqs_cis.view(*shape)
+
+
+def matmul_complex(a: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
+    a_real = a[..., 0]
+    a_imag = a[..., 1]
+    b_real = b[..., 0]
+    b_imag = b[..., 1]
+    return torch.stack([
+        a_real * b_real - a_imag * b_imag, 
+        a_real * b_imag + a_imag * b_real], dim=-1)
 
 
 def apply_rotary_emb(
     xq: torch.Tensor,
     xk: torch.Tensor,
-    freqs_cis: ComplexTensorPair
+    freqs_cis: torch.Tensor
 ) -> Tuple[torch.Tensor, torch.Tensor]:
-    xq_ = _view_as_complex(xq.float().reshape(*xq.shape[:-1], -1, 2))
-    xk_ = _view_as_complex(xk.float().reshape(*xk.shape[:-1], -1, 2))
+    xq_ = xq.float().reshape(*xq.shape[:-1], -1, 2)
+    xk_ = xk.float().reshape(*xk.shape[:-1], -1, 2)
     freqs_cis = reshape_for_broadcast(freqs_cis, xq_)
-    xq_out = _view_as_real(xq_ * freqs_cis).flatten(3)
-    xk_out = _view_as_real(xk_ * freqs_cis).flatten(3)
+    xq_out = matmul_complex(xq_, freqs_cis).flatten(3)
+    xk_out = matmul_complex(xk_, freqs_cis).flatten(3)
     return xq_out.type_as(xq), xk_out.type_as(xk)
 
 
